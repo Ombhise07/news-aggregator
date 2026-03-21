adbd549d564f86450fe977625b08c146f5c4d9ce
Ombhise07 <bhiseom04@gmail.com>
2026-03-21 21:53:21 +0530

    Dispatch fetchNews and display news list

---
+++ news-aggregator-fe/src/pages/Home.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import type { RootState, AppDispatch } from "../app/store";
+import { fetchNews } from "../features/news/newsSlice";
+
+export default function Home() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { news, loading, error } = useSelector(
+    (state: RootState) => state.news
+  );
+
+  useEffect(() => {
+    dispatch(fetchNews());
+  }, [dispatch]);
+
+  if (loading) {
+    return <h2>Loading news...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error: {error}</h2>;
+  }
+
+  return(
+  <div>
+    <h1>Top Headlines</h1>
+
+      {news.map((article) => (
+        <div key={article.id}>
+          <h3>{article.title}</h3>
+          <p>{article.description}</p>
+
+          {/* GNews does NOT have user.name → use source */}
+          <p>Source: {article.source.name}</p>
+        </div>
+      ))}
+  </div>
+  );
+}
