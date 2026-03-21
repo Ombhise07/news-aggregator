ae831042eed55a4ab2c334d542422b4c9c7b9ee9
Ombhise07 <bhiseom04@gmail.com>
2026-03-21 22:16:43 +0530

    Refactor News UI using reusable NewsCard component

--- news-aggregator-fe/src/components/NewsCard.tsx
+++ news-aggregator-fe/src/components/NewsCard.tsx
@@ -1,1 +1,23 @@
- 
+import type{ NewsArticle } from "../features/news/newsSlice";
+
+interface NewsCardProps{
+    article: NewsArticle;
+}
+
+export default function NewsCard({ article }: NewsCardProps){
+    return(
+    <div
+      style={{
+        border: "1px solid #ddd",
+        padding: "16px",
+        marginBottom: "12px",
+      }}
+    >
+        <h3>{article.title}</h3>
+        <p>{article.description}</p>
+        <p>
+            <strong>Source:</strong> {article.source.name}
+        </p>
+    </div>
+    );
+}
