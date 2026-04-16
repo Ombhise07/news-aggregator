9af088adecd63525b3256d5851c748dfc4171045
Ombhise07 <bhiseom04@gmail.com>
2026-04-16 10:36:46 +0530

    Updated auth routes and cleaned project files

--- news-aggregator-be/app/api/routes/auth_routes.py
+++ news-aggregator-be/app/api/routes/auth_routes.py
@@ -16,6 +16,9 @@ from app.services.auth_service import register_user, login_user
 # Importing database session factory
 from app.db.session import SessionLocal
 
+from jose import jwt, JWTError
+from app.core.config import settings
+from app.core.security import create_access_token
 
 # Creating API router
 router = APIRouter()
@@ -60,3 +63,30 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     except Exception as e:
         # Return HTTP 401 for authentication errors
         raise HTTPException(status_code=401, detail=str(e))
+    
+@router.post("/refresh")
+def refresh_access_token(refresh_token: str):
+    try:
+        # Decode refresh token
+        payload = jwt.decode(
+            refresh_token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+
+        email = payload.get("sub")
+        token_type = payload.get("type")
+
+        # Ensure it's a refresh token
+        if email is None or token_type != "refresh":
+            raise HTTPException(status_code=401, detail="Invalid refresh token")
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired refresh token")
+
+    # Generate new access token
+    new_access_token = create_access_token({"sub": email})
+
+    return {
+        "access_token": new_access_token
+    }
