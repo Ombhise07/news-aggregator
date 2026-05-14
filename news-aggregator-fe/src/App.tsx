1cc13d073ca31bdf2e86fd3bc3d9f34f7921a503
Ombhise07 <bhiseom04@gmail.com>
2026-05-14 11:53:47 +0530

    Connected the register page

--- news-aggregator-fe/src/App.tsx
+++ news-aggregator-fe/src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import NewsDetails from './pages/NewsDetails'
 import Home from './pages/Home'
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import Favorites from './pages/Favorites';
 
 
@@ -11,10 +12,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home page */}
         <Route path="/" element={<Home />} />
+
+        {/* NewsDetails page */}
         <Route path="/news/:id" element={<NewsDetails />} />
+
+        {/* Favorites paage */}
         <Route path="/favorites" element={<Favorites />} />
+
+        {/* Login page */}
         <Route path="/login" element={<LoginPage />} />
+
+        {/* Register page */}
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
